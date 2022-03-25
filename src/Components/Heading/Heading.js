76d425dc928bc9bec64ada3fedd24069f9207b84
zereentabassum <islamms262@gmail.com>
2022-03-26 02:07:50 +0600

    heading added

---
+++ src/Components/Heading/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Heading.css'
+
+const Heading = () => {
+    return (
+        <div>
+            <h1 className='heading'>Watch Collections</h1>
+        </div>
+    );
+};
+
+export default Heading;
+  
