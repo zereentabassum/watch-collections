91602246519bd7ee8cbd18c429797e6bcd1edaef
zereentabassum <islamms262@gmail.com>
2022-03-26 18:58:17 +0600

    cart added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = () => {
     return (
         <div className='cart'>
           <h2>Selected Watches</h2>  
+          <button>Choose One For Me</button>
+          <br />
+          <button>Choose Again</button>
         </div>
     );
 };
